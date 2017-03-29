c610fe39780d73a07b417ab3eda97fccced97f06
Paulo Jerônimo <paulojeronimo@gmail.com>
2017-03-29 08:47:54 -0300

    Added javax.persistence.* support

--- src/main/java/org/apache/struts/example/crud/model/Department.java
+++ src/main/java/org/apache/struts/example/crud/model/Department.java
@@ -2,14 +2,29 @@ package org.apache.struts.example.crud.model;
 
 import java.io.Serializable;
 
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.SequenceGenerator;
+import javax.persistence.Table;
+import javax.persistence.UniqueConstraint;
+
+@Entity
+@Table(schema = "STRUTS2_CRUD_EXAMPLE", name = "DEPARTMENT", uniqueConstraints = @UniqueConstraint(columnNames = "NAME"))
 public class Department implements Serializable {
 
-    /**
-     * 
-     */
     private static final long serialVersionUID = 1L;
-    Integer departmentId;
-    String name;
+
+    @Id
+    @SequenceGenerator(name = "SQ_DEPARTMENT", sequenceName="STRUTS2_CRUD_EXAMPLE.SQ_DEPARTMENT", allocationSize = 1)
+    @GeneratedValue(strategy = GenerationType.SEQUENCE, generator = "SQ_DEPARTMENT")
+    @Column(name = "ID", nullable = false, precision = 5)
+    private Integer departmentId;
+
+    @Column(name = "NAME", nullable = false, length = 20)
+    private String name;
 
     public Department() {
     }
--- src/main/java/org/apache/struts/example/crud/model/Employee.java
+++ src/main/java/org/apache/struts/example/crud/model/Employee.java
@@ -2,21 +2,39 @@ package org.apache.struts.example.crud.model;
 
 import java.io.Serializable;
 
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.JoinColumn;
+import javax.persistence.ManyToOne;
+import javax.persistence.SequenceGenerator;
+import javax.persistence.Table;
+
+@Entity
+@Table(schema = "STRUTS2_CRUD_EXAMPLE", name = "EMPLOYEE")
 public class Employee implements Serializable {
 
-    /**
-     * 
-     */
     private static final long serialVersionUID = 1L;
 
+    @Id
+    @SequenceGenerator(name = "SQ_EMPLOYEE", sequenceName="STRUTS2_CRUD_EXAMPLE.SQ_EMPLOYEE", allocationSize = 1)
+    @GeneratedValue(strategy = GenerationType.SEQUENCE, generator = "SQ_EMPLOYEE")
+    @Column(name = "ID", nullable = false, precision = 5)
     private Integer employeeId;
 
+    @Column(name = "AGE", nullable = false)
     private Integer age;
 
+    @Column(name = "FIRST_NAME", nullable = false, length = 10)
     private String firstName;
 
+    @Column(name = "LAST_NAME", nullable = false, length = 10)
     private String lastName;
 
+    @ManyToOne
+    @JoinColumn(name = "DEPARTMENT_ID")
     private Department department;
 
     public Employee() {
